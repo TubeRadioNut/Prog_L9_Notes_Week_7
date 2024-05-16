57c519b32f74c6a1d5ec52aa7e21aac48b7b3cba
rt65s <rt65s@SCHOOL_LAPTOP>
2024-05-16 16:44:04 -0700

    Updated MainWindow.xaml, MainWindow.xaml.cs, Created Order class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,23 +18,17 @@ namespace Prog_L9_Notes_Week_7
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();//<--Don't delete this and keep it at the top of MainWindow()
         }//end of MainWindow
 
 
         //Part 1
         private void btnPart1_Click(object sender, RoutedEventArgs e)
         {
-            
-            
-            
-           
-            
-            
-            //display Info
+            //display the return of FullOrder() method to the label
             lblDisplayInfo.Content = FullOrder();
         }
-
+        //Create the Toppings() method that returns a string
         public string Toppings()
         {
             //check box
@@ -59,75 +53,101 @@ namespace Prog_L9_Notes_Week_7
             toppings += (isPepperoni) ? "Pizza has Pepperoni\n" : "Pizza does Not have Pepperoni\n";
             return toppings;
         }
-
+        //Create the PizzaSize() method that returns a string
         public string PizzaSize()
         {
+            //create a string variable to be displayed in the label
             string size = "";
-            //Radio button
+            //Testing what Radio button is checked with if/else statements
             if ((bool)rbSmall.IsChecked)//If small pizza is checked
             {
+                //Add to string
                 size += "Size Small\n";
             }
             else if ((bool)rbMedium.IsChecked)//If medium pizza is checked
             {
+                //Add to string
                 size += "Size Medium\n";
             }
             else//If larage pizza is checked
             {
+                //Add to string
                 size += "Size Large\n";
             }
+            //return the string varaible
             return size;
         }
-
+        //Create the FullOrder() method that returns a string
         public string FullOrder()
         {
 
-            //Name of the pizza place
-            
-            
+            //Craate string variable for Name of the pizza place
             string pizzaPlaceName = "Charles' Pizza Palace";
-
+            //Create string variable to be displayed in the label
             string order = $"{pizzaPlaceName}\n\n";
-            //the pizza size
+            //Add the pizza size by calling PizzaSize method and add the retrurn to the string variable
             order += PizzaSize();
-            //the toppings
+            //Add the pizza topping by calling Topping method and add the return to the string variable
             order += Toppings();
-
+            //return string variable
             return order;
         }
-
+        //Create the ChangeTheme() click event method
         private void ChangeTheme(object sender, RoutedEventArgs e)
         {
+            //Create List of RadioButtons, name it themes and add the radio buttons to the list
             List<RadioButton> themes = new List<RadioButton>();
             themes.Add(rbWhite);
             themes.Add(rbBlack);
             themes.Add(rbRed);
             themes.Add(rbBlue);
-
+            //Loop through List of radio buttons
             foreach(RadioButton rb in themes)
             {
+                //Testing if a radio button is selected
                 if (rb != null)
                 {
-                    string chosenColor = rb.Content.ToString();
+                    //create string variable to hold content of selected radio button
+                    string chosenColor = "";
+                    //testing which radio button is selected and assigning the content of selected radio button to the string variable
+                    if ((bool)rb.IsChecked)
+                    {
+                        chosenColor = rb.Content.ToString();
+                    }
+                    //testing what content of string variable matches color
                     if (chosenColor == "White")
                     {
+                        //setting background color to selected content of radio button
                         canvasParent.Background = new SolidColorBrush(Colors.White);
                     }
                     else if (chosenColor == "Black")
                     {
+                        //setting background color to selected content of radio button
                         canvasParent.Background = new SolidColorBrush(Colors.Black);
                     }
-                    else if (chosenColor == "Bed")
+                    else if (chosenColor == "Red")
                     {
+                        //setting background color to selected content of radio button
                         canvasParent.Background = new SolidColorBrush(Colors.Red);
                     }
                     else if (chosenColor == "Blue")
                     {
+                        //setting background color to selected content of radio button
                         canvasParent.Background = new SolidColorBrush(Colors.Blue);
                     }
                 }
                 
             }
+        }
+
+        private void btnCreateOrder_Click(object sender, RoutedEventArgs e)
+        {
+            string orderName = txbOrderName.Text;
+            bool isOvernight = cbIsOvernight.IsChecked == true;
+            bool isPerishable = cbIsPerishable.IsChecked == true;
+
+            Order newOrder = new Order(orderName, isOvernight, isPerishable);
+            lblOrderlabel.Content = newOrder.ToString();
         }
     }//end of partial class
 }//end of namespace
---
+++ Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_L9_Notes_Week_7
+{
+    internal class Order
+    {
+        //Fields
+        string _orderName;
+        bool _isOverNight;
+        bool _isPerishable;
+
+        //Constructor
+        public Order(string orderName, bool isOverNight, bool isPerishable)
+        {
+            _orderName = orderName;
+            _isOverNight = isOverNight;
+            _isPerishable = isPerishable;
+        }
+
+        //Properties
+        public string OrderName { get => _orderName; set => _orderName = value; }
+        public bool IsOverNight { get => _isOverNight; set => _isOverNight = value; }
+        public bool IsPerishable { get => _isPerishable; set => _isPerishable = value; }
+
+        //Methods
+
+        public override string ToString()
+        {
+            string formattedString = "";
+            formattedString += $"Name: {_orderName}\n";
+            formattedString += $"Overnight Delivery: {_isOverNight}\n";
+            formattedString += $"Perishable: {IsPerishable}";
+            return formattedString;
+        }
+        
+    }//end of class
+
+}//end of namespace
